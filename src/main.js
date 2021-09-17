d54c1ced0c6b1fdfb44e9846f8c795280bf8cb46
mohammad_saadati <saadati.m.b@gmail.com>
2021-09-17 14:25:14 +0430

    add pwa

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
 import request from "./mixins/req";
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
