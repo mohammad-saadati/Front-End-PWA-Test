5f099380bbeaf1f921424214d357f0468762a755
mohammad_saadati <saadati.m.b@gmail.com>
2021-09-17 12:57:41 +0430

    home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div style="margin-bottom:125px;">
+    <users-list/>
+    <bottom-navigation/>
+  </div>
+</template>
+
+<script>
+import UsersList from "@/components/UsersList";
+import BottomNavigation from "@/components/BottomNavigation";
+
+export default {
+  name: "Home",
+  components: {
+    UsersList,
+    BottomNavigation
+  }
+}
+</script>
