83566b04f1c1e6d0da3ece593c65a3b29b315acd
mohammad_saadati <saadati.m.b@gmail.com>
2021-09-17 12:57:02 +0430

    bottom navigation implementation

---
+++ src/components/BottomNavigation/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bottom-navigation">
+    <router-link to="#" class="home-navigation-item">
+      <img src="@/assets/icons/inbox.png" alt="">
+    </router-link>
+
+    <div class="icons">
+      <router-link to="#">
+        <img class="navigation-item" src="@/assets/icons/inbox.png" alt="">
+      </router-link>
+      <router-link to="#">
+        <img class="navigation-item" src="@/assets/icons/edit.png"  alt="">
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "BottomNavigation"
+}
+</script>
+<style>
+@import "./index.css";
+</style>
