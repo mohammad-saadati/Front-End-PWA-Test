8c97f4e1dd502031c92ddb1730a549e0a9b0e4e3
mohammad_saadati <saadati.m.b@gmail.com>
2021-09-17 13:01:39 +0430

    register mixin and directive to the root instance

--- src/main.js
+++ src/main.js
@@ -1,16 +1,67 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router";
-// import { makeServer } from "./server"
 import request from "./mixins/req";
 
 Vue.config.productionTip = false
 
-// if (process.env.NODE_ENV === "development") {
-//   makeServer()
-// }
-
 Vue.mixin({...request})
+
+Vue.directive('longpress', {
+  bind: function (el, binding, vNode) {
+      // Make sure expression provided is a function
+      if (typeof binding.value !== 'function') {
+          // Fetch name of component
+          const compName = vNode.context.name
+          // pass warning to console
+          let warn = `[longpress:] provided expression '${binding.expression}' is not a function, but has to be`
+          if (compName) { warn += `Found in component '${compName}' ` }
+
+          console.warn(warn)
+      }
+
+      // Define variable
+      let pressTimer = null
+
+      // Define funtion handlers
+      // Create timeout ( run function after 1s )
+      let start = (e) => {
+
+          if (e.type === 'click' && e.button !== 0) {
+              return;
+          }
+
+          if (pressTimer === null) {
+              pressTimer = setTimeout(() => {
+                  // Run function
+                  handler()
+              }, 1000)
+          }
+      }
+
+      // Cancel Timeout
+      let cancel = () => {
+          // Check if timer has a value or not
+          if (pressTimer !== null) {
+              clearTimeout(pressTimer)
+              pressTimer = null
+          }
+      }
+      // Run Function
+      const handler = (e) => {
+          binding.value(e)
+      }
+
+      // Add Event listeners
+      el.addEventListener("mousedown", start);
+      el.addEventListener("touchstart", start);
+      // Cancel timeouts if this events happen
+      el.addEventListener("click", cancel);
+      el.addEventListener("mouseout", cancel);
+      el.addEventListener("touchend", cancel);
+      el.addEventListener("touchcancel", cancel);
+  }
+})
 
 new Vue({
   render: h => h(App),
